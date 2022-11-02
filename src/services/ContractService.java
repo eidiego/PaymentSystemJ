94797dbf61d9c984e1162243b7509d4fbf17f88c
Diego Rodrigues <diegokod@gmail.com>
2022-11-02 16:40:49 -0300

    Feat: Basic service concluded

--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -8,5 +8,17 @@
 
 package services;
 
+import entities.Contract;
+
 public class ContractService {
+    private OnlinePaymentService onlinePaymentService;
+
+
+    public ContractService(OnlinePaymentService onlinePaymentService) {
+        this.onlinePaymentService = onlinePaymentService;
+    }
+
+    public void processContract(Contract contract, int months) {
+
+    }
 }
