87efb8f1bc9dfe0e69b43232dc3d6ce73d091e8e
Diego Rodrigues <diegokod@gmail.com>
2022-11-02 16:39:01 -0300

    Feat: Add interface service

--- src/application/Program.java
+++ src/application/Program.java
@@ -8,6 +8,10 @@
 
 package application;
 
+import entities.Contract;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -16,6 +20,20 @@ public class Program {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyy");
+
+        System.out.println("Entre com os dados do contrato:");
+        System.out.println("Número: ");
+        int number = sc.nextInt();
+        System.out.println("Data (dd/MM/yyyy): ");
+        LocalDate date = LocalDate.parse(sc.next(), fmt);
+        System.out.println("Valor do contrato: ");
+        double totalValue = sc.nextDouble();
+
+        Contract obj = new Contract(number, date, totalValue);
+
+        System.out.println("Número de parcelas: ");
+        int n = sc.nextInt();
 
 
         sc.close();
